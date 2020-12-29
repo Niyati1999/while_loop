f6ff1c40949c600d3955eb47482478c75cf5dbaf
niyati salot <170120107148@git.org.in>
2020-12-29 11:49:58 +0530

    Add project files.

---
+++ while_loop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("print Numbers using while:");
+            /*while(i <= 5){
+                Console.WriteLine(i);
+                i++;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 5);*/
+            string data = "we will do it";
+            int l = data.Length;
+            Console.WriteLine("Length = " + l);
+            char ch = data[0];
+            Console.WriteLine(ch);
+            for(int i = 0; i < l; i++)
+            {
+                char a = data[i];
+                Console.WriteLine(a);
+            }
+        }
+        }
+    }
+
